eca45edc603797cc46132d2f71a6af4144addcbe
GunDeathBulyga <phillbleck@gmail.com>
2019-10-02 13:08:31 +0300

    New list group

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const User = require('../models/User');
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
+const group = []
+
+function onlyUnique(value, index, self) { 
+  return self.indexOf(value) === index;
+}
+
+
+
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
@@ -18,8 +26,14 @@ router.get('/dashboard', ensureAuthenticated,function (req, res){
 });
 
 router.post('/dashboard',function (req, res){
+  
   const usr = req.user;
   const id = usr.id;
+  
+
+  group.push(req.body.groupName);
+
+  var unique = group.filter( onlyUnique );
 
   User.findOne({_id: id}, function(err, foundObject){
     if(foundObject){
@@ -27,14 +41,18 @@ router.post('/dashboard',function (req, res){
         foundObject.groupName = req.body.groupName
 
       foundObject.save(function(err, updatedObject){ 
-        res.render('grouplist', {user: updatedObject}); 
+        res.render('grouplist', {user: updatedObject, groups : unique});  
       })
     }
   })
 })
 
+
+
 router.get('/grouplist', ensureAuthenticated,function (req, res){
   res.render('grouplist')
 });
+
+
 
 module.exports = router;
